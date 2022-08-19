fb2b7abb8d4cbbc00fa15fa922f7f0262f1e4472
Bhaskar031 <bhaskarraju031@gmail.com>
2022-08-19 15:39:12 +0530

    Helloworld

---
+++ Helloworld.java
@@ -0,0 +1,8 @@
+public class Hello world {
+
+   public static void main(String[] args) {
+       // Prints "Hello, world" to the terminal window.
+       System.out.println("Hello, world");
+   }
+   
+}
